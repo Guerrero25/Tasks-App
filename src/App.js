3cc83394f15db23f02cd6c2fe904d14fb8cd6e6c
Omar Guerrero <guerrero9725@gmail.com>
2017-10-21 14:25:36 -0500

    Mejorando estructura, agregando formulario para agregar tasks

--- src/App.js
+++ src/App.js
@@ -6,23 +6,53 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      toDo: ['Hacer deposito', 'Comprar la chocolatina que le debo a mi reina por la apuesta', 'Botar la basura'],
-      Completeds: ['Hacer la tarea', 'Ir a la universidad']
+      toDo: [],
+      Completeds: []
+    }
+
+    this.handleAddTask = this.handleAddTask.bind(this)
+  }
+
+  handleAddTask (e) {
+    e.preventDefault()
+    let input = document.getElementById('new_task')
+    let todo = this.state.toDo
+
+    if (input.value !== '') {
+      const ntask = {
+        id: todo.length,
+        content: input.value
+      }
+      todo.push(ntask)
+      this.setState({
+        toDo: todo
+      })
+      input.value = ''
     }
   }
   render () {
     return (
       <div className='tasks-app'>
-        <ToDo
-          tasks={this.state.toDo}
-          title='Tareas Por Hacer'
-          type='waiting'
-        />
-        <ToDo
-          tasks={this.state.Completeds}
-          title='Tareas Terminadas'
-          type='finish'
-        />
+        <section className='tasks-app__todo'>
+          <ToDo
+            tasks={this.state.toDo}
+            title='Tasks To Do'
+            type='waiting'
+          />
+          <div className='todo__add'>
+            <form action='#' className='form__add'>
+              <input id='new_task' type='text' className='text__form__add' />
+              <input onClick={this.handleAddTask} type='submit' className='submit__form__add' value='new task' />
+            </form>
+          </div>
+        </section>
+        <section className='tasks-app__completed'>
+          <ToDo
+            tasks={this.state.Completeds}
+            title='Task Completed'
+            type='finish'
+          />
+        </section>
       </div>
     )
   }
--- src/todo-component/ToDo.js
+++ src/todo-component/ToDo.js
@@ -2,17 +2,27 @@ import React, {Component} from 'react'
 import './ToDo.css'
 
 class ToDo extends Component {
-  constructor () {
-    super()
+  constructor (props) {
+    super(props)
     this.state = {
-      tasks: ''
+      tasks: []
     }
   }
 
   componentDidMount () {
+    let tasks = this.listTasks(this.props.tasks)
     this.setState({
-      tasks: this.props.tasks.map(task => <li key={task.id} className={'task-item ' + this.props.type} >{task}</li>)
+      tasks: tasks
     })
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (this.props !== nextProps) {
+      let listTasks = this.listTasks(nextProps.tasks)
+      this.setState({
+        tasks: listTasks
+      })
+    }
   }
 
   render () {
@@ -24,6 +34,13 @@ class ToDo extends Component {
         </ul>
       </div>
     )
+  }
+
+  // Helper for build the list's tasks
+  listTasks (tasks) {
+    return tasks.map(task => {
+      return <li key={task.id} className={'task-item ' + this.props.type} >{task.content}</li>
+    })
   }
 }
 
